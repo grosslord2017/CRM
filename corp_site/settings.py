6c17318aea6a742b9204714abcd851edcee93986
grosslord2017 <grosslord2017@ukr.net>
2022-04-30 14:44:47 +0300

    fix

--- corp_site/settings.py
+++ corp_site/settings.py
@@ -15,46 +15,45 @@ from pathlib import Path
 
 
 #--------------------------------
-try:
-    from corp_site.local_setings import *
-except ImportError:
-    SECRET_KEY = 'corpsite_crm'
-
-    BASE_DIR = Path(__file__).resolve().parent.parent
-
-    DEBUG = True
-
-    ALLOWED_HOSTS = ['*']
-
-    # Database
-    # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    print('False')
+# try:
+#     from corp_site.local_setings import *
+# except ImportError:
+#     SECRET_KEY = 'corpsite_crm'
+#
+#     BASE_DIR = Path(__file__).resolve().parent.parent
+#
+#     DEBUG = True
+#
+#     ALLOWED_HOSTS = ['*']
+#
+#     # Database
+#     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+#     print('False')
 #--------------------------------
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-eu$=-%joa00uc_lbadv&j7kfzbdjq6=t6zy#*l$hrp@je&zm5w'
+SECRET_KEY = 'django-insecure-eu$=-%joa00uc_lbadv&j7kfzbdjq6=t6zy#*l$hrp@je&zm5w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,13 +103,15 @@ WSGI_APPLICATION = 'corp_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
