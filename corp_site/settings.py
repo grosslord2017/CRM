cd856549263de95fac06385d36ad8824cdf4468a
grosslord2017 <grosslord2017@ukr.net>
2022-05-04 12:15:47 +0300

    test

--- corp_site/settings.py
+++ corp_site/settings.py
@@ -151,10 +151,11 @@ USE_TZ = True
 
 # STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../corpcrm/static')
+)
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
